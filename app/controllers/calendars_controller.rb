f8e59dbbf235ec1d17e708c8d7a90ffc2c0b0f74
ncn-kim <ncn001001@gmail.com>
2026-05-08 17:20:48 +0900

    issue6 再修正2 キャンセル

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -33,13 +33,13 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-      wday_num = (@todays_date + x).wday
+      wday_num = (@todays_date + x ).wday
+      today_wday = wdays[wday_num]
       if wday_num >= 7
         wday_num = wday_num - 7
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: today_wday }
       @week_days.push(days)
-      today_wday = wdays[wday_num]
     end
 
   end
